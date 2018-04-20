7dac09b382fd18e6d1997d994b5901afc10a98c6
Swolebrain <thecodingteacher@gmail.com>
2018-04-20 15:14:40 -0400

    added put route to insert questions

--- server.js
+++ server.js
@@ -29,23 +29,48 @@ const jwtValidator = jwt({
 app.get('/kotlin/questions', function(req, res){
   console.log("GET /questions");
   Question.find({})
-    .sort({createdDate: -1})
+    // .sort({createdDate: -1})
     .then(qs=>res.json(qs))
-    .catch(ress=>console.log(err));
+    .catch(err=>res.status(500).json(err));
 });
 
-app.post('/kotlin/questions', jwtValidator,  function(req, res){
+app.post('/kotlin/questions', jwtValidator,  emailValidator, function(req, res){
   console.log('POST /questions');
-  if (!req.user.email.endsWith('@triplebyte.com') && 
-    !req.user.email.endsWith('@toptal.com') &&
-    req.user.email.toLowerCase() !== 'fermartel@gmail.com') 
-    return res.status(401).json([{question: 'Unauthorized', _id: 1}]);
+
   const q = new Question(req.body);
   q.createdBy = req.user.email;
   q.createdDate = new Date().getTime();
   q.save()
     .then(dbRes => res.json(dbRes))
-    .catch(ress=>console.log(err));
+    .catch(err=>res.status(500).json(err));
 });
+
+app.put('/kotlin/questions/:id', jwtValidator, emailValidator, async (req, res) => {
+  const {id:qid} = req.params;
+  console.log(`PUT /questions/${qid}`);
+  try{
+    const existingQuestion = await Question.findOne({_id: qid});
+    if (!existingQuestion) return res.status(400).json({err: 'couldn\'t find that id'});
+
+    for (let key in req.body) existingQuestion[key] = req.body[key];
+
+    const saveResult = await existingQuestion.save();
+    if (!saveResult) throw 'Error while saving to db';
+    res.json({saveResult});
+  }
+  catch(err){
+    console.log(err);
+    res.status(500).json({err});
+  }
+});
+
+function emailValidator(req, res, next){
+    if (!req.user.email.endsWith('@triplebyte.com') &&
+        !req.user.email.endsWith('@toptal.com') &&
+        req.user.email.toLowerCase() !== 'fermartel@gmail.com' &&
+        req.user.email.toLowerCase() !== 'imoreno138@gmail.com')
+        return res.status(401).json([{question: 'Unauthorized', _id: 1}]);
+    next();
+}
 
 app.listen(1338, console.log('App listening....'));
